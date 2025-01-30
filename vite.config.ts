385f12d0e5cf70fea5a48b3eea79425aae90db87
alexandrosandro <93431268+alexandrosandro@users.noreply.github.com>
2025-01-29 21:59:24 -0300

    Add files via upload
    Sistema Ordem de serviço - Deploy

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Sistema de Gestão',
+        short_name: 'SGestão',
+        description: 'Sistema de Gestão de Produtos e Serviços',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff'
+      }
+    })
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: process.env.ELECTRON == "true" ? './' : '/',
+}));
